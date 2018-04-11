410008e4fc4b9aac129f4bdb19d4ae1a8c3d7260
Marton Szucs <marton@tempusinfo.com>
2018-04-11 19:01:32 +0200

    bumping version to 0.0.2

--- src/main/java/martonsz/teleportdb/Ref.java
+++ src/main/java/martonsz/teleportdb/Ref.java
@@ -3,7 +3,7 @@ package martonsz.teleportdb;
 public class Ref {
 	public static final String MODID = "teleport_database";
 	public static final String NAME = "Teleport Database";
-	public static final String VERSION = "0.0.1";
+	public static final String VERSION = "0.0.2";
 	public static final String CLIENT_PROXY = "martonsz.teleportdb.proxy.ClientProxy";
 	public static final String SERVER_PROXY = "martonsz.teleportdb.proxy.ServerProxy";
 	public static final String ACCEPTED_REMOTE_VERSION = "*";;
